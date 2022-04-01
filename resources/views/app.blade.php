98e4054199bd87f7581959772712fe2a9b6589d2
Raihan Adam <hallwack.id@gmail.com>
2022-04-01 13:22:09 +0700

    :tada: First Commit - :heavy_plus_sign: Add TailwindCSS - :bento: Add Assets

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Village Futsal</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline">
+        Anjaaayyy
+    </h1>
+</body>
+
+</html>
